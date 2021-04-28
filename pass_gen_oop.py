5f9b551beb13f27dcce51c177a7245b08b1ec590
Gillian Law <gillianlaw@techliterate.co.uk>
2021-04-28 17:27:41 +0200

    Checks length of password requested

--- pass_gen_oop.py
+++ pass_gen_oop.py
@@ -3,15 +3,6 @@ import sqlite3
 import string
 
 """An attempt to rewrite the password generator using oop principles"""
-# def passwordlength():
-#     length = 0
-#     while 6 < length < 10:
-#         try:
-#             n = int(input('Enter your password length (minimum 6, maximum 10): '))
-#         except ValueError:
-#             print('Not a number')
-#     return length
-
 
 def create_password(passwordlength):
 
@@ -19,8 +10,6 @@ def create_password(passwordlength):
     but I want to ensure there is some of each type in each password.
     I've chosen (for the moment) to make each password 10 digits long
     rather than offer a choice"""
-
-
 
     p1 = "".join(random.sample(string.ascii_lowercase, 2))
     p2 = "".join(random.sample(string.ascii_uppercase, 2))
@@ -35,15 +24,35 @@ def create_password(passwordlength):
     random_password = "".join(random.sample(all, passwordlength))#TODO consider offering choice of length
     return random_password
 
+#TODO the database stuff isn;t working...
+# def save_to_db():
+#     db = sqlite3.connect('passwords1.sqlite')
+#     db.execute("CREATE TABLE IF NOT EXISTS passwordsOOP (name TEXT, password TEXT)")
+#     db.execute("INSERT INTO passwordsOOP (name, password) VALUES (?, ?)", (main.website, main.password))
+#
+#     update_sql = "SELECT * FROM passwordsOOP"
+#     cursor = db.cursor()
+#     cursor.execute(update_sql)
+#
+#     cursor.connection.commit()
+#     cursor.close()
+#     db.close()
 
 def main():
     website = input("Which site is this for? ")
     passwordlength = int(input("How many characters (min 6, max 10)? "))
+    # TODO the below should prob be in a sep function
+    if 5 < passwordlength < 10:
+        length = passwordlength
+    else:
+        length = 10
+        print("Password must be between 6 and 10 characters. I have given you 8.")
 
 
-    password = create_password(passwordlength)
-
+    password = create_password(length)
     print('Generated password for {}: {}'.format(website, password))
+
+    # save_to_db()
 
 run = True
 while run:
