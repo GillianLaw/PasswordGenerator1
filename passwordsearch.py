d35ae0ab96c07fbcc9e607528e53a7076b3cee56
Gillian Law <gillianlaw@techliterate.co.uk>
2021-05-16 16:17:01 +0200

    Adding search ability

---
+++ passwordsearch.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+def lookup():
+    db = sqlite3.connect('passwordOOP.sqlite')
+
+    search = input("Which site are you looking for? ")
+    for row in db.execute("SELECT * FROM passwordOOP WHERE name LIKE ?", (search,)):
+        # print(row)
+        print("The password for {} is {}".format(row[0], row[1]))
+
+    # for name, password in db.execute("SELECT * FROM passwords"):
+    #     print(name)
+    #     print(password)
+    #     print("-" * 20)
+    # print("The password for {} is {}".format(row[0], row[1]))
+
+    db.close()
+
+hunt = lookup()
+print(hunt)
