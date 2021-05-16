17aaea29bc78d9e12c0e8b55d79c580ac477ead1
Gillian Law <gillianlaw@techliterate.co.uk>
2021-05-16 16:15:40 +0200

    Got the database working

--- pass_gen_oop.py
+++ pass_gen_oop.py
@@ -6,26 +6,22 @@ import string
 
 def create_password(passwordlength):
 
-    """I know I could use 'string.ascii_letters + string.punctuation'
-    but I want to ensure there is some of each type in each password.
-    ""
-
     p1 = "".join(random.sample(string.ascii_lowercase, 2))
     p2 = "".join(random.sample(string.ascii_uppercase, 2))
     p3 = "".join(random.sample(string.digits, 3))
     p4 = "".join(random.sample(string.punctuation, 3))
 
-    """Add these together in a string and then randomise again.
-    I don't want a password that is lower case first, then upper, etc"""
+# Add these together then randomise again.
+# Don't want a password lower case first, then upper, etc
 
     all = p1 + p2 + p3 + p4
 
-    random_password = "".join(random.sample(all, passwordlength))#TODO consider offering choice of length
+    random_password = "".join(random.sample(all, passwordlength))
     return random_password
 
-#TODO the database stuff isn;t working...
-def save_to_db():
-    db = sqlite3.connect('passwords.sqlite')
+
+def save_to_db(website, password):
+    db = sqlite3.connect('passwordOOP.sqlite')
     db.execute("CREATE TABLE IF NOT EXISTS passwordOOP (name TEXT, pass TEXT)")
     db.execute("INSERT INTO passwordOOP (name, pass) VALUES (?, ?)", (website, password))
 
@@ -52,12 +48,8 @@ def main():
     password = create_password(length)
     print('Generated password for {}: {}'.format(website, password))
 
-    save_to_db()
+    save_to_db(website, password)
+    # working!!
 
-run = True
-while run:
-    try:
-        main()
-    except KeyboardInterrupt:
-        run = False
-        print('Ctrl+c caught exiting')
+experiment = main()
+print(experiment)
